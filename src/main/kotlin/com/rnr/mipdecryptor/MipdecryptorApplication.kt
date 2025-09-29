3bc309762200d9175838ce4c1ff6f45e66376ad7
Farihim <mzadhirul.farihim-shamsuddin@edag.com>
2025-09-29 18:38:07 +0800

    initial commit

---
+++ src/main/kotlin/com/rnr/mipdecryptor/MipdecryptorApplication.kt
@@ -0,0 +1,11 @@
+package com.rnr.mipdecryptor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MipdecryptorApplication
+
+fun main(args: Array<String>) {
+	runApplication<MipdecryptorApplication>(*args)
+}
